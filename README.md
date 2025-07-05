1184a55073ae269656eb7394cd3f5b185b8ab584
andrei <35298550+d4rckh@users.noreply.github.com>
2025-07-05 19:43:42 +0300

    instructions

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Serba
 
 HTTP File server written in Micronaut
+
+### minimal compose
+
+```yaml
+services:
+  backend:
+    image: ghcr.io/d4rckh/serba:latest
+    container_name: serba-backend
+    volumes:
+      - /my_data_folder:/data
+    ports:
+      - "8080:8080"
+    environment:
+      JDBC_URL: jdbc:postgresql://serba-postgres:5432/serba
+      DB_USERNAME: serba
+      DB_PASSWORD: serba
+    depends_on:
+      - serba-postgres
+
+  serba-postgres:
+    image: postgres:15
+    container_name: serba-postgres
+    environment:
+      POSTGRES_DB: serba
+      POSTGRES_USER: serba
+      POSTGRES_PASSWORD: serba
+    restart: unless-stopped
+```
+
+After accessing the login page, an admin user will be created and it's password will be printed in the container logs, as soon as you login go to /admin and change your admin user's password. There, you will also be able to create a library.
--- build-docker.bat
+++ build-docker.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
+cd frontend
+pnpm build
+
+cd ..
+
 REM Step 1: Copy frontend/dist to Micronaut public folder
 echo Copying frontend/dist to src/main/resources/public...
 if exist src\main\resources\public (
@@ -24,6 +29,27 @@ if errorlevel 1 (
     exit /b 1
 )
 
-echo Docker image serba-jvm:latest built successfully!
+echo Docker image serba-backend:latest built successfully!
+
+REM Step 3: Tag the image for GitHub Container Registry
+echo Tagging Docker image for ghcr.io...
+docker tag serba-backend:latest ghcr.io/d4rckh/serba:latest
+
+if errorlevel 1 (
+    echo Docker tag failed.
+    exit /b 1
+)
+
+REM Step 4: Push the image to GitHub Container Registry
+REM echo Pushing Docker image to ghcr.io...
+REM docker push ghcr.io/d4rckh/serba:latest
+
+REM if errorlevel 1 (
+REM     echo Docker push failed.
+REM     exit /b 1
+REM )
+
+REM echo Docker image pushed to ghcr.io/d4rckh/serba:latest successfully!
+
 endlocal
 pause
